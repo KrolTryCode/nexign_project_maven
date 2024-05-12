4f4ff0abe13742a88f64f9175e3eeab54f7d97ca
simplex <amalsk0905@yandex.u>
2024-05-13 00:27:09 +0300

    add javadoc

--- src/main/java/nexign_project_maven/cdr/Main.java
+++ src/main/java/nexign_project_maven/cdr/Main.java
@@ -1,9 +1,7 @@
 package nexign_project_maven.cdr;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class Main {
